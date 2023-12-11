85a12e5f547c6188ec6003da720ff5e193f9a88c
chikaho <147366528+chikaho@users.noreply.github.com>
2023-12-11 16:48:14 +0900

    Delete mdn.js

--- mdn.js
+++
@@ -1,8 +0,0 @@
-const date = new Date();
-const [ year,month, day, ] = [
-  date.getFullYear(),
-  date.getMonth() + 1,
-  date.getDate(), 
-];
-const dateText = year + '年' + month + '月' + day + '日';
-console.log(dateText);
